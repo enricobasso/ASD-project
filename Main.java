fc29925e65853670d1ecca662a0efa0a24f1b47a
Enrico Basso <enrico.basso01@gmail.com>
2019-04-25 19:19:24 +0200

    Switched from the static idea to the attribute + methods idea.

--- Main.java
+++ Main.java
@@ -1,32 +1,54 @@
 import static java.lang.System.*;
 
 public class Main {
+    double[] array;
+
+    public Main(double[] input) {
+        this.array = input;
+    }
+
     public static void main(String[] args) {
         /*
          Array initialization
          @TODO: input from System.in
          */
         double[] input = {0.1, 0.35, 0.05, 0.1, 0.15, 0.05, 0.2};
+        /*
+            0.05
+            0.05
+            0.1
+            0.1
+            0.15
+            0.2
+            0.35
+        */
         for (int i = 0; i < input.length; i++) {
             System.out.println(input[i]);
         }
 
-        System.out.println("-> " + randomizedSelect(input, 0, input.length - 1, 6));
+        Main media = new Main(input);
+        int pivot = media.select(0, media.array.length - 1, media.array.length / 2);
+        System.out.println("-> pivot: " + pivot);
+        /*media.swapCells(pivot, media.array.length - 1);
+        media.partition(0, media.array.length - 1);
+
+        for (int i = 0; i < media.array.length; i++) {
+            System.out.println(media.array[i]);
+        }*/
 
     }
 
     /**
      * Return the sum of the specified elements of the array.
-     * @param array the array containing the elements to sum
      * @param pos1 the index of the first element to sum
      * @param pos2 the index of the last element to sum
      * @return the sum
      */
-    public static double arraySum(double[] array, int pos1, int pos2) {
+    public double arraySum(int pos1, int pos2) {
         double sum = 0;
 
         for (int i = pos1; i <= pos2; i++) {
-            sum = sum + array[i];
+            sum = sum + this.array[i];
         }
 
         return sum;
@@ -35,70 +57,54 @@ public class Main {
     /**
      * Swap
      */
-    public static double[] swapCells(double[] array, int i, int j) {
+    public void swapCells(int i, int j) {
         double temp;
 
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-
-        return array;
+        temp = this.array[i];
+        this.array[i] = this.array[j];
+        this.array[j] = temp;
     }
 
     /**
      * Partition algorithm
      *
      */
-    public static int partition(double[] array, int p, int r) {
-        double x = array[r];
+    public int partition(int p, int r) {
+        double x = this.array[r];
         int i = p - 1;
 
         for (int j = p; j < r; j++) {
-            if (array[j] <= x) {
+            if (this.array[j] <= x) {
                 i++;
-                array = swapCells(array, i, j);
+                this.swapCells(i, j);
             }
         }
-        array = swapCells(array, i + 1, r);
+        this.swapCells(i + 1, r);
 
         return i + 1;
     }
 
     /**
-     * Randomized partition
-     */
-    public static int randomizedPartition(double[] array, int p, int r) {
-        /*int i = random(p, r);
-        array = swapCells(array, r, i);*/
-
-        return partition(array, p, r);
-    }
-
-    /**
      * Select algorithm
+     * @return the index of the element found
      */
-    public static double randomizedSelect(double[] array, int p, int r, int i) {
+    public int select(int p, int r, int i) {
         int q, k;
 
         if (p == r)
-            return array[p];
+            //return array[p];
+            return p;
 
-        q = partition(array, p, r);
+        q = this.partition(p, r);
         k = q - p + 1;
 
         if (i == k)
-            return array[q];
+            //return array[q];
+            return q;
         else if (i < k)
-            return randomizedSelect(array, p, q - 1, i);
+            return this.select(p, q - 1, i);
         else
-            return randomizedSelect(array, q + 1, r, i - k);
+            return this.select(q + 1, r, i - k);
 
     }
-
-    /**
-     * Random number generator
-     */
-    /*public static int random(int seed) {
-
-    }*/
 }
